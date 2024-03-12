f217d624f56d7f49339006f22a4f2e0daa7b7094
vinibressanini <vinimarcus41@gmail.com>
2024-03-12 18:48:46 -0300

    test: created a couple of tests for the onBoarding repo implementation

---
+++ test/src/on_boarding/data/repositories/on_boarding_repository_impl_test.dart
@@ -0,0 +1,56 @@
+import 'package:dartz/dartz.dart';
+import 'package:education_app/core/errors/exceptions.dart';
+import 'package:education_app/core/errors/failure.dart';
+import 'package:education_app/src/on_boarding/data/datasources/on_boarding_local_datasource.dart';
+import 'package:education_app/src/on_boarding/data/repositories/on_boarding_repository_impl.dart';
+import 'package:education_app/src/on_boarding/domain/repositories/on_boarding_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class OnBoardingLocalDatasourceMock extends Mock
+    implements OnBoardingLocalDatasource {}
+
+void main() {
+  late OnBoardingLocalDatasource localDatasource;
+  late OnBoardingRepositoryImpl repo;
+
+  setUp(() {
+    localDatasource = OnBoardingLocalDatasourceMock();
+    repo = OnBoardingRepositoryImpl(localDatasource);
+  });
+
+  test('[localDatasource] should be a subclass of [OnBoardingRepository]', () {
+    expect(repo, isA<OnBoardingRepository>());
+  });
+
+  group('cacheFirstTimer tests', () {
+    test(
+        'WHEN [localDatasource.cacheFirstTimer()] call is successfull THEN return a [Rigth(null)]',
+        () async {
+      when(() => localDatasource.cacheFirstTimer())
+          .thenAnswer((_) async => Future.value());
+
+      final result = await repo.cacheFirstTimer();
+
+      expect(result, equals(const Right<dynamic, void>(null)));
+      verify(() => localDatasource.cacheFirstTimer()).called(1);
+      verifyNoMoreInteractions(localDatasource);
+    });
+
+    test(
+        'WHEN [localDatasource.cacheFirstTimer()] call is unsuccessfull THEN return a [Left(CacheFailure)]',
+        () async {
+      when(() => localDatasource.cacheFirstTimer())
+          .thenThrow(const CacheException(message: "Insufficient storage"));
+
+      final result = await repo.cacheFirstTimer();
+
+      expect(
+          result,
+          equals(Left<CacheFailure, void>(
+              CacheFailure(message: "Insufficient storage", statusCode: 500))));
+      verify(() => localDatasource.cacheFirstTimer()).called(1);
+      verifyNoMoreInteractions(localDatasource);
+    });
+  });
+}
