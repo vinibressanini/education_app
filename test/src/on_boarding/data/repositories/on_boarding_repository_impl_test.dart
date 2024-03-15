a1a486fc0f06839a5c352c4cd9953b9d7fb0448f
vinibressanini <vinimarcus41@gmail.com>
2024-03-15 18:20:30 -0300

    test : created tests for onBoarding repo checkUser method

--- test/src/on_boarding/data/repositories/on_boarding_repository_impl_test.dart
+++ test/src/on_boarding/data/repositories/on_boarding_repository_impl_test.dart
@@ -22,7 +22,6 @@ void main() {
   test('[localDatasource] should be a subclass of [OnBoardingRepository]', () {
     expect(repo, isA<OnBoardingRepository>());
   });
-
   group('cacheFirstTimer tests', () {
     test(
         'WHEN [localDatasource.cacheFirstTimer()] call is successfull '
@@ -54,6 +53,52 @@ void main() {
         ),
       );
       verify(() => localDatasource.cacheFirstTimer()).called(1);
+      verifyNoMoreInteractions(localDatasource);
+    });
+  });
+
+  group('checkIfUserIsFirstTimer', () {
+    test('MUST return a [Right(true)] if the user is first timer', () async {
+      when(() => localDatasource.checkIfUserIsFirstTimer())
+          .thenAnswer((_) async => true);
+
+      final result = await repo.checkIfUserIsFirstTimer();
+
+      expect(result, equals(const Right<dynamic, bool>(true)));
+      verify(() => localDatasource.checkIfUserIsFirstTimer()).called(1);
+      verifyNoMoreInteractions(localDatasource);
+    });
+
+    test('MUST return a Right(false) if the user is not a first timer',
+        () async {
+      when(() => localDatasource.checkIfUserIsFirstTimer())
+          .thenAnswer((_) async => false);
+
+      final result = await repo.checkIfUserIsFirstTimer();
+
+      expect(result, equals(const Right<dynamic, bool>(false)));
+      verify(() => localDatasource.checkIfUserIsFirstTimer()).called(1);
+      verifyNoMoreInteractions(localDatasource);
+    });
+
+    test(
+        'MUST return a Left(CacheFailure) '
+        'if the datasource call is unsuccessful', () async {
+      when(() => localDatasource.checkIfUserIsFirstTimer()).thenThrow(
+        const CacheException(message: 'Insufficient storage'),
+      );
+
+      final result = await repo.checkIfUserIsFirstTimer();
+
+      expect(
+        result,
+        equals(
+          Left<CacheFailure, dynamic>(
+            CacheFailure(message: 'Insufficient storage', statusCode: 500),
+          ),
+        ),
+      );
+      verify(() => localDatasource.checkIfUserIsFirstTimer()).called(1);
       verifyNoMoreInteractions(localDatasource);
     });
   });
